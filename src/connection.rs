ae7fe4bd99c0132cc22ab76628efb8367682a103
Jesse Xu <jesse@5th.work>
2022-03-29 21:05:45 +0800

    Debug

--- src/connection.rs
+++ src/connection.rs
@@ -700,8 +700,7 @@ async fn run_connection(state: &mut NSQDConnectionState) -> Result<(), Error> {
     let identify_body = IdentifyBody {
         client_id: state.config.shared.client_id.clone(),
         feature_negotiation: true,
-//        tls_v1: state.config.shared.tls.is_some(),
-        tls_v1: true,
+        tls_v1: state.config.shared.tls.is_some(),
         sample_rate: state.config.sample_rate.map(|rate| rate.get()),
         deflate: matches!(
             state.config.shared.compression,
